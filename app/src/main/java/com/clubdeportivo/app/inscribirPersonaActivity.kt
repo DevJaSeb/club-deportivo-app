a1765058969eac85c292655ad4b63419f6b0300b
celina <celinatarn96@gmail.com>
2026-05-07 18:26:42 -0300

    inscribir persona

---
+++ app/src/main/java/com/clubdeportivo/app/inscribirPersonaActivity.kt
@@ -0,0 +1,14 @@
+package com.clubdeportivo.app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class inscribirPersonaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inscribir_persona)
+    }
+}
