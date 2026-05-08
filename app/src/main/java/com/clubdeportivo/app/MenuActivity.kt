14a56a73271884a335679650d0ede4009092631e
celina <celinatarn96@gmail.com>
2026-05-07 22:19:51 -0300

    layout completar inscripcion activity

--- app/src/main/java/com/clubdeportivo/app/MenuActivity.kt
+++ app/src/main/java/com/clubdeportivo/app/MenuActivity.kt
@@ -11,13 +11,7 @@ import androidx.core.view.WindowInsetsCompat
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_menu)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.menu)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
         val btnSalir = findViewById<Button>(R.id.btn_salir)
         btnSalir.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
