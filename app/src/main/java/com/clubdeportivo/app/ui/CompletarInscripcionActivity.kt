9338f36f762e51a3e29bc9c86f67bf848cb7f9d3
celina <celinatarn96@gmail.com>
2026-05-09 23:41:33 -0300

    Buenas prácticas

--- app/src/main/java/com/clubdeportivo/app/ui/CompletarInscripcionActivity.kt
+++ app/src/main/java/com/clubdeportivo/app/ui/CompletarInscripcionActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Button
 import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
 import com.clubdeportivo.app.R
+import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 class CompletarInscripcionActivity : AppCompatActivity() {
@@ -16,7 +17,8 @@ class CompletarInscripcionActivity : AppCompatActivity() {
         setContentView(R.layout.activity_completar_inscripcion)
 
         val rgTipoSocio = findViewById<RadioGroup>(R.id.rg_tipo_socio)
-        val tilActividades = findViewById<TextInputLayout>(R.id.lbl_actividades)
+        val tilActividades = findViewById<TextInputLayout>(R.id.til_actividades)
+        val etActividades = findViewById<TextInputEditText>(R.id.et_actividades)
         val flechaVolver = findViewById<ImageView>(R.id.btn_volver)
         val btnFinalizarInscripcion = findViewById<Button>(R.id.btn_finalizar_inscripcion)
 
@@ -36,14 +38,17 @@ class CompletarInscripcionActivity : AppCompatActivity() {
             when (checkedId) {
                 R.id.rdo_socio -> {
                     tilActividades.isEnabled = false
+                    etActividades.isEnabled = false
 
                     // Limpiar el texto si había algo seleccionado
-                    findViewById<AutoCompleteTextView>(R.id.lbl_actividades).text.clear()
+                    // findViewById<AutoCompleteTextView>(R.id.lbl_actividades).text.clear()
+                    etActividades.text?.clear()
                 }
 
                 R.id.rdo_noSocio -> {
                     // Si NO es Socio, lo volvemos a activar
                     tilActividades.isEnabled = true
+                    etActividades.isEnabled = true
                 }
             }
         }
